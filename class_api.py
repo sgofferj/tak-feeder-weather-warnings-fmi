da7a2f2d4f8b6e13df6db6b41bce72ec15165e69
sgofferj <stefan@gofferje.net>
2025-07-31 14:41:27 +0300

    Cleanup unused functions

--- class_api.py
+++ class_api.py
@@ -21,36 +21,6 @@ class server:
         else:
             return r.status_code, r.json()
 
-    def getMissionRole(self, name):
-        """Returns role in the mission"""
-        path = f"/Marti/api/missions/{name}/role"
-        url = self.apiBaseURL + path
-        r = req.get(url, cert=self.crt, verify=False)
-        if r.status_code != 200:
-            return r.status_code, r.text
-        else:
-            return r.status_code, r.json()
-
-    def getMissionSubscriptions(self, name):
-        """Returns subscribtions to the mission"""
-        path = f"/Marti/api/missions/{name}/subscriptions"
-        url = self.apiBaseURL + path
-        r = req.get(url, cert=self.crt, verify=False)
-        if r.status_code != 200:
-            return r.status_code, r.text
-        else:
-            return r.status_code, r.json()
-
-    def getMissionSubscriptionRoles(self, name):
-        """Returns subscribtions to the mission"""
-        path = f"/Marti/api/missions/{name}/subscriptions/roles"
-        url = self.apiBaseURL + path
-        r = req.get(url, cert=self.crt, verify=False)
-        if r.status_code != 200:
-            return r.status_code, r.text
-        else:
-            return r.status_code, r.json()
-
     def createMission(
         self, name, creatorUid, group="", defaultrole="", classification=""
     ):
@@ -87,18 +57,6 @@ class server:
         url = self.apiBaseURL + path
         r = req.put(url, cert=self.crt, verify=False)
         if r.status_code != 201:
-            return r.status_code, r.text
-        else:
-            return r.status_code, r.json()
-
-    def setMissionRole(self, name, clientUid, userName, role, token):
-        """Sets the role for a subscriber"""
-        path = f"/Marti/api/missions/{name}/role"
-        headers = {"Authorization": f"Bearer {token}"}
-        data = {"clientUid": clientUid, "username": userName, "role": role}
-        url = self.apiBaseURL + path
-        r = req.put(url, cert=self.crt, headers=headers, verify=False)
-        if r.status_code != 200:
             return r.status_code, r.text
         else:
             return r.status_code, r.json()
